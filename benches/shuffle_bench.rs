c01866183a7f0a247da5d5a0eed7c03e16385fc5
ruescasd <fastness@gmail.com>
2020-11-02 17:07:01 +0100

    ristretto bench

--- benches/shuffle_bench.rs
+++ benches/shuffle_bench.rs
@@ -2,6 +2,7 @@ use rand::Rng;
 use rand_core::{OsRng};
 use rug::Integer;
 use serde::{Deserialize, Serialize};
+use std::time::Duration;
 
 use rmxmg::arithm::*;
 use rmxmg::group::*;
@@ -11,35 +12,10 @@ use rmxmg::rug_b::*;
 use rmxmg::hashing;
 use rmxmg::hashing::{HashBytes, HashTo};
 use rmxmg::shuffle::*;
-use std::time::{Instant};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion, SamplingMode, BenchmarkId};
 
-struct GenProofIn<'a, E: Element> {
-    es: &'a Vec<Ciphertext<E>>,
-    e_primes: &'a Vec<Ciphertext<E>>, 
-    r_primes: &'a Vec<E::Exp>, 
-    perm: &'a Vec<usize>, 
-    pk: &'a dyn PublicK<E, OsRng>, 
-    generators: &'a Vec<E>, 
-    hasher: &'a dyn HashTo<E::Exp>
-}
-struct CheckProofIn<'a, E: Element> {
-    proof: &'a Proof<E, E::Exp>, 
-    es: &'a Vec<Ciphertext<E>>,
-    e_primes: &'a Vec<Ciphertext<E>>, 
-    pk: &'a dyn PublicK<E, OsRng>, 
-    generators: &'a Vec<E>, 
-    hasher: &'a dyn HashTo<E::Exp>
-}
-
-fn gen_proof_f<E: Element>(i: &GenProofIn<E>) -> Proof<E, E::Exp> {
-    gen_proof(i.es, i.e_primes, i.r_primes, i.perm, i.pk, i.generators, i.hasher)
-}
-fn check_proof_f<E: Element>(i: &CheckProofIn<E>) -> bool {
-    check_proof(i.proof, i.es, i.e_primes, i.pk, i.generators, i.hasher)
-}
-fn shuffle_f(n: usize) -> bool {
+fn shuffle_rug(n: usize) -> bool {
     let group = RugGroup::default();
     let exp_hasher = &*group.exp_hasher();
     let csprng = OsRng;
@@ -66,70 +42,49 @@ fn shuffle_f(n: usize) -> bool {
     ok
 }
 
-use std::time::Duration;
-
-fn criterion_benchmark(c: &mut Criterion) {
-    let group = RugGroup::default();
-    let exp_hasher = &*group.exp_hasher();
+fn shuffle_ristretto(n: usize) -> bool {
     let csprng = OsRng;
-        
-    let sk = group.gen_key_conc(csprng);
-    let pk = sk.get_public_key_conc();
-    let n = 300;
-    
-    let mut es: Vec<Ciphertext<Integer>> = Vec::with_capacity(n);
-    
+    let group = RistrettoGroup;
+    let exp_hasher = &*group.exp_hasher();
+
+    let sk2 = group.gen_key(csprng);
+    let pk2 = sk2.get_public_key();
+
+    let mut es = Vec::with_capacity(10);
+
     for _ in 0..n {
-        let plaintext: Integer = group.encode(group.rnd_exp(csprng));
-        let c = pk.encrypt(plaintext, csprng);
+        let plaintext = group.rnd(csprng);
+        let c = pk2.encrypt(plaintext, csprng);
         es.push(c);
     }
-        
+
     let hs = generators(es.len() + 1, &group);
-        
-    let (e_primes, rs, perm) = gen_shuffle(&es, &pk);
+    let (e_primes, rs, perm) = gen_shuffle(&es, &*pk2);
+    let proof = gen_proof(&es, &e_primes, &rs, &perm, &*pk2, &hs, exp_hasher);
+    let ok = check_proof(&proof, &es, &e_primes, &*pk2, &hs, exp_hasher);
 
-    let i1 = GenProofIn {
-        es: &es,
-        e_primes: &e_primes,
-        r_primes: &rs,
-        perm: &perm,
-        pk: &pk,
-        generators: &hs,
-        hasher: exp_hasher
-    };
+    assert!(ok == true);
 
-    let proof = gen_proof(&es, &e_primes, &rs, &perm, &pk, &hs, exp_hasher);
+    ok
+}
 
-    let i2 = CheckProofIn {
-        proof: &proof,
-        es: &es,
-        e_primes: &e_primes,
-        pk: &pk,
-        generators: &hs,
-        hasher: exp_hasher
-    };
-
+fn criterion_benchmark(c: &mut Criterion) {
     
     let mut group = c.benchmark_group("shuffle");
     group
         .sample_size(10)
         .measurement_time(Duration::from_secs(60))
         .sampling_mode(SamplingMode::Flat);
-    /*
-    group.bench_function("gen_proof", |b| 
-        b.iter(|| (gen_proof_f(black_box(&i1))))
-    );
-    group.bench_function("check_proof", |b| 
-        b.iter(|| (check_proof_f(black_box(&i2))))
-    );
-    */
-    for size in [200].iter() {
-        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            b.iter(|| shuffle_f(size));
+    for size in [100].iter() {
+        group.bench_with_input(BenchmarkId::new("shuffle_rug", size), size, |b, &size| {
+            b.iter(|| shuffle_rug(size));
         });
     }
-    
+    for size in [1000].iter() {
+        group.bench_with_input(BenchmarkId::new("shuffle_ristretto", size), size, |b, &size| {
+            b.iter(|| shuffle_ristretto(size));
+        });
+    }
     group.finish();
 }
 
